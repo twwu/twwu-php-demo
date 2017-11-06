f0e9d6aed42a0b7799b9bbc40b16fc71eb613041
twwu <twwu@twwudeMac-mini.local>
2017-11-06 18:21:19 +0800

    controller

---
+++ controllers/Login.php
@@ -0,0 +1,48 @@
+<?php
+    class Login {  
+
+        private $account = '';
+
+        private $password = '';
+      
+        function __construct($content){
+            $input = json_decode($content,true);
+
+            if (isset($input['account'])) {
+                $this->account = $input['account'];
+            } 
+
+            if (isset($input['password'])) {
+                $this->password = $input['password'];
+            } 
+              
+        }  
+        public function index()  
+        {  
+            if ($this->account == null || strlen($this->account) <= 0){
+                return ;
+            }  
+
+            if ($this->password == null || strlen($this->password) <= 0){
+                return ;
+            }
+
+            return 'printer_logical_fontheight(printer_handle, height)';
+        }  
+
+        private function generateTokens(){
+        if ($this->account == null || strlen($this->account) <= 0)
+            return null;
+
+        if ($this->password == null || strlen($this->password) <= 0)
+            return null;
+
+        $timestamp = date("YmdHis");
+
+        $token = sha1($this->account . $this->password . $timestamp);
+
+        return $token;
+
+    }
+    }  
+?>
--- controllers/User.php
+++ controllers/User.php
@@ -1,12 +1,12 @@
 <?php
     class User {  
       
-    function __construct(){  
-          
+        function __construct(){  
+              
+        }  
+        public function index($name='')  
+        {  
+            echo 'hello,'.$name.',lucky,you are arrive here!';  
+        }  
     }  
-    public function index($name='')  
-    {  
-        echo 'hello,'.$name.',lucky,you are arrive here!';  
-    }  
-}  
 ?>
